ef1ce74f61df2dc58c6187ef62da944acedb50a3
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-29 16:02:34 +0700

    While

--- 10001st Prime/Source.cpp
+++ 10001st Prime/Source.cpp
@@ -3,6 +3,15 @@
 #include<math.h>
 int main()
 {
+	int num[4]; // num[0] = answer  num[1] =number  num[2] =count  num[3] = flag
+	num[3] = num[2] = 0;
+	num[1] = num[0] = 1;
+	while (num[2]<1001)
+	{
+		num[3] = 0; // current number is a prime number
+
+
+	}
 
 
 
